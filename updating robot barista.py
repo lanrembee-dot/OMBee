0c7b5007c2baecc9c7636e75ab7e4371640e3a7f
Laredo <lanrembee@gmail.com>
2026-02-21 03:11:05 +0100

    Update robot barista script and cleanup checkpoints

---
+++ updating robot barista.py
@@ -0,0 +1,18 @@
+# Let's start a coffee shop together!! We're going to build a coffee shop using some new python programming concepts!!
+
+# Let's build robot Barista!! 
+
+print("Hello, welcome to Larryhuba coffee Shop !!!!")
+
+name = input("What is your name? \n")
+
+if name == "Ben":
+        print("You are not welcome here Evil Ben . Get out !!!")
+        exit()
+else:
+        print("Hello " + name + ",thank you so much for coming in today.\n ")
+
+menu = "Black Coffee, Espresso, Lattee, Cappuchino"
+
+print(name + ",What would you like from our menu today? Here is what we are serving.\n\n" + menu)
+
